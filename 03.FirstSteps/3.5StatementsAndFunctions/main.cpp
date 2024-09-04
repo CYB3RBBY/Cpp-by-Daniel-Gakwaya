f0c7d774b071c4077586e9e0f3a9087092e3397a
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-09-04 14:36:13 +0530

    learn statements and functions in c language

--- 03.FirstSteps/3.5StatementsAndFunctions/main.cpp
+++ 03.FirstSteps/3.5StatementsAndFunctions/main.cpp
@@ -1,29 +1,38 @@
+// Statements and functions in c++
+
 #include <iostream>
 
-int addNumbers( int first_param, int second_param){
-    int result = first_param + second_param;
+// a statement is a basic unit of computation in a c++ program
+// every c++ program is a collection of statements organized in a certain way to achieve same goal
+// Statements end with a semicolon in C++(;)
+
+// Statements are executed in order from top to bottom when the program is run
+
+// Execution keeps going until there is a statement causing the program to terminate, or run another sequence of statements
+
+int addNumbers(int firstParameter, int secondParameter)
+{
+    int result = firstParameter + secondParameter;
     return result;
 }
 
-int main(){
+int main(int argc, char **argv)
+{
+    int firstNumber{10}; // Statement
+    int secondNumber{4};
 
-    int first_number {13}; // Statement
-    int second_number {7};
+    std::cout << "First Number:" << firstNumber << std::endl;
+    std::cout << "Second Number:" << secondNumber << std::endl;
 
-    std::cout << "First number : " << first_number << std::endl; // 13
-    std::cout << "Second number : " << second_number << std::endl; // 7
+    int sum = firstNumber + secondNumber;
+    std::cout << "Sum:" << sum << std::endl;
 
-    int sum = first_number + second_number;
-    std::cout << "Sum : " << sum << std::endl; // 20
+    sum = addNumbers(10, 4);
+    std::cout << "Sum:" << sum << std::endl;
+    sum = addNumbers(34, 58);
+    std::cout << "Sum:" << sum << std::endl;
 
-    sum = addNumbers(25,7);
-    std::cout << "Sum : " << sum << std::endl; // 32
-
-    sum = addNumbers(30,54);
-    std::cout << "Sum : " << sum << std::endl;
-
-    std::cout << "Sum : " << addNumbers(3,42) << std::endl;
-
+    std::cout << "Sum :" << addNumbers(12, 13) << std::endl;
 
     return 0;
 }
